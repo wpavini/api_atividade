876b5b5e926305d0ef75977e2437f768a3edb6a5
wpavini <62146804+wpavini@users.noreply.github.com>
2020-04-05 10:50:56 -0300

    adicionando models com tabelas de pessoas e atividades e criado em utils de teste para manipular os dados do banco de dados

---
+++ utils.py
@@ -0,0 +1,33 @@
+from models import Pessoas
+
+def insere_pessoas():
+   pessoa = Pessoas(nome='Carla', idade=36)
+   print(pessoa)
+   pessoa.save()
+
+def consulta_pessoas():
+    pessoa = Pessoas.query.all()
+    pessoa = Pessoas.query.filter_by(nome='Carla').first()
+    print(pessoa.idade)
+
+def altera_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Carla') .first()
+    pessoa.nome = 35
+    pessoa.save()
+
+def exclui_pessoa():
+    pessoa = Pessoas.query.filter_by(nome='Felipe').first()
+    pessoa.delete()
+
+
+
+
+if __name__ == '__main__':
+    insere_pessoas()
+    altera_pessoa()
+    exclui_pessoa()
+    consulta_pessoas()
+
+
+
+
